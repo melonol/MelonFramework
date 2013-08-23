15b30b94836bb0b9bd0f14164a9b21edd983b24a
Melon <397574898@qq.com>
2013-08-23 22:47:36 +0800

    second commit

---
+++ melon.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Melon;
+define( 'IN_MELON', true );
+
+class Melon {
+	
+	final static public function app() {
+		
+	}
+}
+DB('table')->query();
+DB::query();
+
+class DB {
+	
+	function __construct() {
+		echo 'melon db';
+	}
+}
