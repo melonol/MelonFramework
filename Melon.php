76b648ba3b13c43d424af80b64cbe24305b3785a
Melon <397574898@qq.com>
2013-10-10 00:09:06 +0800

    核心文件Melon.php添加初始化功能 － loader进行中

--- Melon.php
+++ Melon.php
@@ -5,9 +5,14 @@ define( 'IN_MELON', true );
 class Melon {
 	
 	static protected $_loaderSet;
-
-	private $_autoload = array(
+	
+	static protected $_permission;
+	
+	static protected $_includePath = array(
+		__DIR__
 	);
+
+	static protected $_autoload;
 
 	final protected function __construct() {
 		;
@@ -26,24 +31,59 @@ class Melon {
 			$melonRoot . 'Helper' . DIRECTORY_SEPARATOR . 'RecursiveSet.php',
 			$melonRoot . 'File' . DIRECTORY_SEPARATOR . 'PathTrace.php',
 			$melonRoot . 'File' . DIRECTORY_SEPARATOR . 'LoaderSet.php',
+			$melonRoot . 'File' . DIRECTORY_SEPARATOR . 'LoaderPermission.php',
 		) );
 		
 		define( 'MELON_INIT', true );
 	}
 	
 	static private function _initLoader( $autoload ) {
-        $scripts = array();
+		$scripts = array();
 		foreach( $autoload as $script ) {
 			require $script;
-            $scripts[ $script ] = true;
+			$scripts[ $script ] = true;
 		}
 		self::$_loaderSet = new \Melon\File\LoaderSet( $scripts,
 			\Melon\File\LoaderSet::REPLACE_NOT );
+		self::$_permission = new \Melon\File\LoaderPermission( self::$_includePath );
 		spl_autoload_register( '\Melon::autoload' );
 	}
 
-	final static public function load( $file ) {
-		return PathTrace::parse( $file, false );
+	final static public function load( $script ) {
+		$load = \Melon\File\PathTrace::parse( $script, true );
+		if( ! $load ) {
+			throw new \Melon\Exception\RuntimeException( "无法识别{$script}脚本，请检查脚本是否存在" );
+		}
+		if( ! is_file( $load['target'] ) ) {
+			throw new \Melon\Exception\RuntimeException( "{$script}不是一个文件" );
+		}
+		if( ! self::$_loaderSet->has( $load['target'] ) ) {
+			if( ! self::$_permission->verify( $load['source'], $load['target'] ) ) {
+				throw new \Melon\Exception\RuntimeException( "{$load['source']}脚本没有权限载入{$load['target']}脚本" );
+			}
+			include $load['target'];
+			self::$_loaderSet->set( $load['target'], true );
+		}
+	}
+	
+	final static public function acquire( $script ) {
+		$load = \Melon\File\PathTrace::parse( $script, true );
+		if( ! $load ) {
+			// TODO::改为抛出警告
+			throw new \Melon\Exception\RuntimeException( "无法识别{$script}脚本，请检查脚本是否存在" );
+		}
+		if( ! is_file( $load['target'] ) ) {
+			// TODO::改为抛出警告
+			throw new \Melon\Exception\RuntimeException( "{$script}不是一个文件" );
+		}
+		if( ! self::$_permission->verify( $load['source'], $load['target'] ) ) {
+			// TODO::改为抛出警告
+			throw new \Melon\Exception\RuntimeException( "{$load['source']}脚本没有权限载入{$load['target']}脚本" );
+		}
+		return ( include $load['target'] );
+	}
+	
+	final static public function read(  ) {
 	}
 	
 	final static public function autoLoad( $class ) {
@@ -73,4 +113,3 @@ class cms extends Melon {
 		parent::init();
 	}
 }
-Melon::init();
