2cfe714ebd143d505de92f62c1439493d31ad6e3
Melon <397574898@qq.com>
2014-01-07 22:18:57 +0800

    增加数据库模型类

---
+++ Melon/Database/Model.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Melon － 可用于php5.3或以上的开源框架
+ * 
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @link http://git.oschina.net/397574898/MelonFramework
+ * @author Melon <denglh1990@qq.com>
+ * @version 0.1.0
+ */
+
+namespace Melon\Database;
+
+use Melon\Exception;
+defined('IN_MELON') or die('Permission denied');
+
+class Model {
+	
+}
