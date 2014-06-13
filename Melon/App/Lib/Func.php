2eb36f6a1bd96b34133650bc3a6622afb1947d66
Melon <397574898@qq.com>
2014-06-13 11:37:39 +0800

    更新版本至0.2.3

--- Melon/App/Lib/Func.php
+++ Melon/App/Lib/Func.php
@@ -5,7 +5,7 @@
  * @license http://www.apache.org/licenses/LICENSE-2.0
  * @link http://framework.melonol.com
  * @author Melon <admin@melonol.com>
- * @version 0.2.2
+ * @version 0.2.3
  */
 
 namespace Melon\App\Lib\Func;
